13e601299ea68d60e2cf7490256e1ba4b060fe64
abdusSamadJIMS <abdussamadjims@gmail.com>
2024-06-10 18:28:56 +0530

    first commit

---
+++ src/components/Cross.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cross = () => {
+    return (
+        <div className='absolute top-2 md:left-4 left-0 '>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 cursor-pointer">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+        </div>
+    )
+}
+
+export default Cross
